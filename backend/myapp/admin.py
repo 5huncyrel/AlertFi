1b089285b5b2d2ef7b83dbecb6a7218d3a7ad500
Shun Cyrel <caseres.shun@gmail.com>
2025-05-19 11:46:14 +0800

    Backend Added

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AlertLog
 
-# Register your models here.
+@admin.register(AlertLog)
+class AlertLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'alert_level', 'message', 'timestamp')
+    search_fields = ('user__username', 'message')
+    list_filter = ('alert_level', 'timestamp')
