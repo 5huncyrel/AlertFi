33417d0569536719a590fc3b7b9a0d075482fb4f
Shun Cyrel <caseres.shun@gmail.com>
2025-05-19 10:55:04 +0800

    Backend Added

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -2,11 +2,11 @@
 
 
 from django.urls import path
-from .views import RegisterView, LoginView, ProtectedView, AlertLogListView
+from .views import RegisterView, LoginView, ProtectedView, AlertLogListCreateView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('protected/', ProtectedView.as_view()),
-    path('alerts/', AlertLogListView.as_view()),  
+    path('alerts/', AlertLogListCreateView.as_view()),  
 ]
