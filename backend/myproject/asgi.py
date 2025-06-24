fdd62e40306dfbe9b18cfe9e97340fc1d37d1e9f
Shun Cyrel <caseres.shun@gmail.com>
2025-06-24 20:10:57 +0800

    Changes on Backend

--- backend/myproject/asgi.py
+++ backend/myproject/asgi.py
@@ -9,26 +9,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 # myproject/asgi.py
 
-# myproject/asgi.py
-
 import os
-import django
-
-# Set Django settings and setup before importing Django apps/middleware
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-django.setup()
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from myapp.middleware import JWTAuthMiddleware
-from myapp.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleware(
-        AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        )
-    ),
-})
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+application = get_asgi_application()
