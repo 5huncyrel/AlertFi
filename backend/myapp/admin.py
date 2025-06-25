9229344b7c03efe61b4139ceb1fed3f0b769a87f
Shun Cyrel <caseres.shun@gmail.com>
2025-06-25 13:01:09 +0800

    Admin Updated

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -1,3 +1,5 @@
+# myapp/admin.py
+
 from django.contrib import admin
 from .models import User, Detector, DetectorReading
 
@@ -7,6 +9,9 @@ class DetectorReadingAdmin(admin.ModelAdmin):
     list_filter = ('detector',)
     search_fields = ('detector__name',)
 
+@admin.register(Detector)
+class DetectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'sensor_on', 'user')
+    readonly_fields = ('sensor_on',)  
 
 admin.site.register(User)
-admin.site.register(Detector)
