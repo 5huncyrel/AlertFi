d9d9b38bca5ee0bf2ab1d8ff2de3ef643ebea01f
Shun Cyrel <caseres.shun@gmail.com>
2025-10-15 02:07:05 +0800

    Backend Changes

--- backend/myapp/serializers.py
+++ backend/myapp/serializers.py
@@ -9,10 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'full_name','notifications_enabled', 'address')
 
+
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admin
         fields = ['id', 'email', 'full_name']
+        
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
@@ -22,14 +24,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'full_name', 'address')
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            full_name=validated_data.get['full_name'],
-            address=validated_data.get['address']
-        )
-        return user
+        return User.objects.create_user(**validated_data)
 
 
 class DetectorSerializer(serializers.ModelSerializer):
